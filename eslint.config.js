e0ba79f7a821bf436300fa43d47d7f56b3be20c8
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-09 00:31:52 +0800

    chore: upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,7 @@
 import { sxzz } from '@sxzz/eslint-config'
-export default sxzz([], { vue: true })
+export default sxzz(
+  {
+    rules: { 'sxzz/require-package-field': 'error' },
+  },
+  { vue: true },
+)
